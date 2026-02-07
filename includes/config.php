6e049c7f1c01edd40f595a99d1b55cb6a3a33bbf
dev7oi <ytrfhjncd4rg66g@gmail.com>
2026-02-07 20:33:03 +0300

    Update config for Railway

--- includes/config.php
+++ includes/config.php
@@ -1,29 +1,32 @@
-
 <?php
-// +------------------------------------------------------------------------+
-// | @author: MvnThemes
-// | @name: Zontal - The Arcade Online HTML5 Game Playing Platform
-// | @author_email: mvk62015@gmail.com   
-// | @version: 1.0v
-// +------------------------------------------------------------------------+
-// | Zontal - The Arcade Online HTML5 Game Playing Platform
-// | Copyright (c) 2017 Zontal. All rights reserved.
-// +------------------------------------------------------------------------+
+// Railway configuration
+if (getenv('RAILWAY_ENVIRONMENT')) {
+    $sql_db_host = getenv('MYSQLHOST') ?: 'localhost';
+    $sql_db_user = getenv('MYSQLUSER') ?: 'root';
+    $sql_db_pass = getenv('MYSQLPASSWORD') ?: '';
+    $sql_db_name = getenv('MYSQLDATABASE') ?: 'railway';
+    $sql_db_port = getenv('MYSQLPORT') ?: '3306';
 
-@header("location: install/index");
-die();
+    $railway_domain = getenv('RAILWAY_PUBLIC_DOMAIN');
+    if ($railway_domain) {
+        $site_url = "https://" . $railway_domain . "/";
+    } else {
+        $site_url = "https://" . getenv('RAILWAY_STATIC_URL') . "/";
+    }
+} else {
+    // إعدادات الاتصال المحلية
+    $sql_db_host = "localhost";
+    $sql_db_user = "root";
+    $sql_db_pass = "";
+    $sql_db_name = "zontal_db";
+    $sql_db_port = "3306";
+    $site_url = "http://localhost/";
+}
 
-// MySQL Hostname
-$sql_db_host = "YOUR_HOST";
-// MySQL Database User
-$sql_db_user = "YOUR_DATABASE_USER";
-// MySQL Database Password
-$sql_db_pass = "YOUR_DATABASE_PASSWORD";
-// MySQL Database Name
-$sql_db_name = "YOUR_DATABASE_NAME";
+// إنشاء اتصال بقاعدة البيانات مع تحديد المنفذ
+$con = mysqli_connect($sql_db_host, $sql_db_user, $sql_db_pass, $sql_db_name, $sql_db_port);
 
-$con = mysqli_connect($sql_db_host, $sql_db_user, $sql_db_pass, $sql_db_name);
-
-// Site URL
-$site_url = "http://example.com/"; // e.g (http://example.com)
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
